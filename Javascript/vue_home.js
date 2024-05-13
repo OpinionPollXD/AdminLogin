19606ad51a756a78c6745dfc4113b4c1394a4e5f
Harun Hursid <harun.hursid@hotmail.com>
2024-05-13 14:09:15 +0200

    arbejdet på vue men det virker ikke

---
+++ Javascript/vue_home.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+const baseURL = "https://restopinionpoll.azurewebsites.net/api/Questions";
+
+
+
+Vue.createApp({
+    data() {
+        return {
+            questions: [],
+            newQuestion: "",
+        };
+    },
+    created() {
+        this.getAllQuestions(baseURL);
+    },
+    methods: {
+        async getAllQuestions(url) {
+            try {
+                const response = await axios.get(url);
+                this.questions = await response.data;
+                console.log(this.questions);
+            }
+            catch (error) {
+                alert(error.message);
+                console.log(error);
+            }
+
+        },
+    },
+}).mount("#app");
