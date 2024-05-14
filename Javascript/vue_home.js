df49d4f5ee09429bf89856523b1aad53171b0510
MetteTobiasen <tobiasen_1@hotmail.com>
2024-05-14 10:12:56 +0200

    addQuestion virker og gemmer i databasen korrekt

--- Javascript/vue_home.js
+++ Javascript/vue_home.js
@@ -5,7 +5,9 @@ Vue.createApp({
     data(){
         return {
             questions: [],
-            newQuestion: "",
+            newQuestion: { questionText: "", category: "", option1: "", option2: "", option3: "", option1Count: 0, option2Count: 0, option3Count: 0 },
+            addMessage: ""
+
         }
     }, 
     created() {
@@ -24,5 +26,16 @@ Vue.createApp({
             }
 
         },
+        async addQuestion() {
+            try{
+                response = await axios.post(baseURL, this.newQuestion)
+                this.addMessage = " response " + response.status + " " + response.statusText
+                this.getAllQuestions(baseURL);
+            }
+            catch (error) {
+                alert(error.message);
+                console.log(error);
+            }
+        }
     }
 }).mount("#app")
